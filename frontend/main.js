46636366196dafc56b1ca389d4f168cdfcc64cca
Andrew Foster <andy@sol1.com.au>
2022-08-06 19:10:47 +1000

    Front-end ingredients

--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,15 @@ import './style.css'
 import { setupCounter } from './counter.js'
 import cytoscape from 'cytoscape';
 
+/**
+ * TODO
+ * - Persist state in localStorage (eventually server)
+ * - Ability to add a new node to the graph from an external element
+ * - Ability to select an existing node and add a new downstream node with an edge
+ * - Ability to click on a node and delete it, deleting all dependent edges
+ * - Ability to click on a node and add an edge to another existing node
+ */
+
 document.querySelector('#app').innerHTML = `
   <div>
     <div class="card">
@@ -13,76 +22,45 @@ document.querySelector('#app').innerHTML = `
 `
 
 setupCounter(document.querySelector('#counter'))
-addNode(document.querySelector('#add-c'))
 
-export function addNode(element) {
-  element.addEventListener('click', () => {
+var toJson = function(obj) { return obj.json() }
+var toText = function(obj) { return obj.text() }
+
+// Fetch starter elements then initialise Cytoscape
+var graphP = fetch('elements.json').then(toJson);
+var styleP = fetch('style.cycss').then(toText);
+
+Promise.all([ graphP, styleP ]).then((then) => {
+  var elements = then[0];
+  var style = then[1];
+
+  var cy = cytoscape({
+    container: document.getElementById('cy'),
+    layout: {
+      name: 'grid'
+    },
+    elements: elements,
+    style: style
+  })
+  
+  cy.on('select', 'node', function(evt) {
+    var node = evt.target;
+    console.log( 'selected ' + node.id() );
+    node.addClass('has-been-selected')
+  })
+
+  cy.on('unselect', 'node', function(evt) {
+    var node = evt.target;
+    console.log( 'unselected ' + node.id() );
+  })
+
+  document.querySelector('#add-c').addEventListener('click', () => {
     cy.add([
       { group: 'nodes', data: { id: 'c', label: 'C' } },
       { group: 'edges', data: { id: 'ac', source: 'a', target: 'c' } },
+      { group: 'edges', data: { id: 'bd', source: 'b', target: 'd' } },
+      { group: 'edges', data: { id: 'db', source: 'd', target: 'b' } },
     ])
     cy.fit()
   })
-}
-
-var cy = cytoscape({
-  container: document.getElementById('cy'),
-
-  elements: [ // list of graph elements to start with
-    { // node a
-      data: {
-        id: 'a',
-        label: 'A'
-      }
-    },
-    { // node b
-      data: {
-        id: 'b',
-        label: 'B'
-      }
-    },
-    { // node b
-      data: {
-        id: 'd',
-        label: 'D'
-      }
-    },
-    { // edge ab
-      data: { id: 'ab', source: 'a', target: 'b' }
-    }
-  ],
-
-  style: [ // the stylesheet for the graph
-    {
-      selector: 'node',
-      style: {
-        'background-color': '#666',
-        'label': 'data(label)'
-      }
-    },
-
-    {
-      selector: 'edge',
-      style: {
-        'width': 3,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle',
-        'curve-style': 'bezier'
-      }
-    }
-  ]
-});
-
-cy.layout({
-  name: 'grid',
-  fit: true
 })
-
-cy.on('tap', 'node', function(evt) {
-  var node = evt.target;
-  console.log( 'tapped ' + node.id() );
-  node.style({
-    'background-color': 'blue'
-  })
-});
