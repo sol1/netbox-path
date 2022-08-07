5a3d40ce1492b20e99269b075d92e4f34251957e
Andrew Foster <andy@sol1.com.au>
2022-08-07 18:00:46 +1000

    Define static files. Base view template.

--- netbox_path/urls.py
+++ netbox_path/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import models, views
 from netbox.views.generic import ObjectChangeLogView
+from django.conf import settings
+from django.conf.urls.static import static
 
-urlpatterns = (
+urlpatterns = [
     path('paths/', views.PathListView.as_view(), name='path_list'),
     path('paths/add/', views.PathEditView.as_view(), name='path_add'),
     path('paths/<int:pk>/', views.PathView.as_view(), name='path'),
@@ -11,4 +13,4 @@ urlpatterns = (
     path('paths/<int:pk>/changelog/', ObjectChangeLogView.as_view(), name='path_changelog', kwargs={
         'model': models.Path
     }),
-)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
